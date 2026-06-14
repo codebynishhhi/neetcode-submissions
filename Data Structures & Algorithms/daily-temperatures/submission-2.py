4e8bc4291e70241b2836f70ef566113f96520719
Mini <nishhhi.tech@gmail.com>
2026-06-14 13:53:37 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Brute force - 
+        # T.C = O(n^2), S.C = O(N)
+        newlist = [0] * len(temperatures)
+        stack = []
+        # for i in range(len(temperatures)):
+        #     for j in range(i+1,len(temperatures)):
+        #         if temperatures[j] > temperatures[i]:
+        #             # distance between j - 1 is actuall number of days
+        #             # between the current to next warmer day
+        #             newlist[i] = j-i
+        #             break
+        # return newlist 
+
+        # Optimized solution using MONOTONIC STACK
+        # T.C = O(N), S.C = O(N)
+        # enumerate bcz we are iterating & storing key value pair
+        # distance between j - 1 is actuall number of days here
+        # here j = prev_index
+        for i, curr_temp in enumerate(temperatures):
+            # while stack is not empty and current_temp > existing
+            while stack and curr_temp > stack[-1][1]:
+
+                prev_index, prev_temp = stack.pop()
+
+                newlist[prev_index] = i - prev_index
+
+            stack.append((i, curr_temp))
+
+        return newlist
+
+                
+            
+
+        
