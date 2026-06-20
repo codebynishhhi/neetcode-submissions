9480dbea9f71631ba9d24fd661c715c14e2eef79
Mini <nishhhi.tech@gmail.com>
2026-06-20 17:02:35 +0530

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# using Dummy Node Problem 
+# 1 dummy node and 1 pointer tail
+# 1. create a fake node to attach all the ans
+# 2. tail initially poits to dummy , then keeps increasing always pointing to last node
+# 3. attach smaller value to dummy
+# 4. move tail pointer
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val <= list2.val :
+                tail.next = list1
+                list1 = list1.next 
+            else:
+                tail.next = list2
+                list2 = list2.next
+
+            # point the tail of next to new value
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+        return dummy.next
+
+        
